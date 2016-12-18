994ccaa29ee1e9b3011b621f7f693b77633b2531
Daniel Oliveira Braga <daniel_oliveira0@hotmail.com>
2016-12-18 12:49:30 -0200

    remove assets

---
+++ public/assets/users-22d19717e41197fb64679e9326674136a2110e13a44b4837e1f3e7ff0bbb8931.js
@@ -0,0 +1,1 @@
+function f1(){1==document.getElementById("radio1").checked?document.getElementById("pdf").disabled=!1:document.getElementById("pdf").disabled=!0,1==document.getElementById("radio2").checked&&(document.getElementById("pdf").disabled=!0)}
